00f8d9c44204c7654859ab05da1025b348f4c1c0
kangdonhee <kangdonhee@192.168.0.4>
2015-11-20 00:17:08 +0900

    add mySampleList

--- sample-01/src/main/java/x/y/z/MyCoinController.java
+++ sample-01/src/main/java/x/y/z/MyCoinController.java
@@ -1,23 +1,35 @@
 package x.y.z;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import x.y.z.service.MyCoinService;
 
 @Controller
 public class MyCoinController {
 
+	@Autowired
+	MyCoinService myCoinService;
+	
+	
 	@RequestMapping(value="/my/sample")
-	public String mySample() {
+	public String mySample(ModelMap model) {
 		// 긴급 패치
 		/**
 		 * 패치 사항을 꼭 테스트 해 주세요.
 		 * 
 		 * 
 		 */
+		
+		List<Map<String, Object>> list = myCoinService.getSampleList();
+		model.addAttribute("list", list);
 		return "page1";
 	}
 	
---
+++ sample-01/src/main/java/x/y/z/service/MyCoinService.java
@@ -0,0 +1,10 @@
+package x.y.z.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MyCoinService {
+
+	List<Map<String, Object>> getSampleList();
+
+}
