a444231df774f84b8da28406eadfa4dd8a380476
saytokang@gmail.com <saytokang@gmail.com>
2021-04-03 22:54:51 +0900

    add lombok

--- sample-01/src/main/java/x/y/z/HooController.java
+++ sample-01/src/main/java/x/y/z/HooController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class HooController {
 
 	@RequestMapping(value="/hoo/1")
 	@ResponseBody
 	public String hoo(){
+		
 		return "my hoo";
 	}
 }
